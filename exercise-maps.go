1ea6971cb8a949afb04b6815d652503a9a9c35da
nob.murakita <nobmurakita@users.noreply.github.com>
2018-01-08 23:15:14 +0900

    exercise maps

---
+++ exercise-maps.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "strings"
+    "golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+    wc := make(map[string]int)
+    for _, word := range strings.Fields(s) {
+        wc[word]++
+    }
+    return wc
+}
+
+func main() {
+    wc.Test(WordCount)
+}
